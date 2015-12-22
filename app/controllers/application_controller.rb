300a1ea2cb1ea58ac32fbb822c6202faf271090f
jmoe <me@jmoe.com>
2015-12-21 22:47:00 -0800

    refactoring featured trip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
+require 'open-uri'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def featured_trip
+    @featured_trip = JSON.load(open('http://api.outerspatial.com/v0/trips/1250.json'))
+    render partial: '/pages/featured_trip', locals:{trip:@featured_trip}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'destinations#index'
   get "/yosemite" => 'high_voltage/pages#show', id: 'yosemite'
   get "/to/:id(/from/:from)" => "high_voltage/pages#show", as: :to
+  get "/index/featured_trip" => "application#featured_trip"
 end
